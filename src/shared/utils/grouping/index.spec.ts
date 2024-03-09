8a6bec1ff1b27d841a3dc7f953c1e555ad8bf0e6
Alfredo Tang <asdzxcca0617@gmail.com>
2024-03-09 21:19:14 +0800

    fix: fix unit test

--- src/shared/utils/grouping/index.spec.ts
+++ src/shared/utils/grouping/index.spec.ts
@@ -1,19 +1,39 @@
 import grouping from '.'
 
+vi.mock('nanoid', () => ({ nanoid: () => 'nanoid' }))
+
 const source = [1, 2, 3, 4, 5]
 
-const expectResult: Record<number, Array<Array<number>>> = {
-  1: [[1, 2, 3, 4, 5]],
+const expectResult: Record<number, unknown> = {
+  1: [{ key: 'nanoid', value: [1, 2, 3, 4, 5] }],
   2: [
-    [1, 3, 5],
-    [2, 4],
+    { key: 'nanoid', value: [1, 3, 5] },
+    { key: 'nanoid', value: [2, 4] },
   ],
-  3: [[1, 4], [2, 5], [3]],
-  4: [[1, 5], [2], [3], [4]],
-  5: [[1], [2], [3], [4], [5]],
+  3: [
+    { key: 'nanoid', value: [1, 4] },
+    { key: 'nanoid', value: [2, 5] },
+    { key: 'nanoid', value: [3] },
+  ],
+  4: [
+    { key: 'nanoid', value: [1, 5] },
+    { key: 'nanoid', value: [2] },
+    { key: 'nanoid', value: [3] },
+    { key: 'nanoid', value: [4] },
+  ],
+  5: [
+    { key: 'nanoid', value: [1] },
+    { key: 'nanoid', value: [2] },
+    { key: 'nanoid', value: [3] },
+    { key: 'nanoid', value: [4] },
+    { key: 'nanoid', value: [5] },
+  ],
 }
 
 describe('grouping', () => {
+  beforeAll(() => {
+    vi.clearAllMocks()
+  })
   source.forEach(item => {
     describe(`${item} groups`, () => {
       it('is correct', () => {
